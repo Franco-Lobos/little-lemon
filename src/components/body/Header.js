938df8560f391d900c3e4d3d41405a7715a2ae42
Franco-Lobos <francolobos3@gmail.com>
2023-07-27 23:11:56 +0200

    headerLogoLInk

--- src/components/body/Header.js
+++ src/components/body/Header.js
@@ -4,9 +4,14 @@ import logo from '../../img/Asset9.png'
 
 
 const Header = ()=>{
+
+    const goHome=()=>{
+        window.location.href = '/';
+    }
+    
     return(
         <header>
-            <img src={logo} alt="company logo"  className="header-logo"></img>
+            <img src={logo} alt="company logo"  className="header-logo" onClick={goHome}></img>
             <Nav></Nav>
         </header>
     )
